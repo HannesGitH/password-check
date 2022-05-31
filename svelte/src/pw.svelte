e7e0591a5dec4935b4f272dff533166a9cb32a3c
HannesH <33062605+HannesGitH@users.noreply.github.com>
2022-05-31 15:57:10 +0200

    added password visibility toggle

--- svelte/src/pw.svelte
+++ svelte/src/pw.svelte
@@ -1,9 +1,29 @@
 <script lang="ts">
   import { password, security } from "./pw_store";
   export let withAsterix: Boolean = false;
+
+  let show_password = false;
+  $: type = show_password ? "text" : "password";
+  $: cla = show_password ? "fa-eye" : "fa-eye-slash";
+	
+  function onInput (event) {
+    $password = event.target.value
+  }
+
 </script>
 
-<input type="password" bind:value={$password} />
+<div id="input-wrapper">
+  <input 
+    {type} 
+    value={$password} 
+    on:input={ onInput } 
+    style="
+      font-weight: {show_password ? 'normal' : 'bold'}; 
+      letter-spacing : {show_password ? '0' : '3px'}
+    "
+  /> 
+  <i class="{cla} fas" on:click={ () => show_password = !show_password } />
+</div>
 
 <p>
   it takes
@@ -14,17 +34,36 @@
 </p>
 
 <style>
+  #input-wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+    height: 100%;
+  }
   input {
+    /* font-size: 16em; */
+    /* font: small-caption; */
+    /* font-family: Roboto; */
     margin: 0 auto;
     width: 100%;
     outline: none;
     text-align: center;
     border: none;
+    border-radius: 15px;
   }
+
+  i {
+    font-size: 1.5em;
+    cursor: pointer;
+    padding: 0.5em;
+  }
+
   input:focus {
   }
 
-  b{
+  b {
     font-size: large;
     font-weight: bolder;
     text-transform: uppercase;
